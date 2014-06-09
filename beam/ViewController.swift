f1b0e687bbab2d2d1e312735a410043eb2dc132f
Capybara <git.capybara@gmail.com>
2014-06-09 12:35:50 -0400

    Initial Commit

---
+++ beam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  beam
+//
+//  Created by Jason P Allen on 6/9/14.
+//  Copyright (c) 2014 Jason P Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
